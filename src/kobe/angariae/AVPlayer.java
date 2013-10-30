89a8986b397ec933f9c718aa297cbf0bf8ca8b84
wombatatat <renagadesoffunk3@gmail.com>
2013-10-30 13:02:45 -0400

    changes made to AVPlayer methods

--- src/kobe/angariae/AVPlayer.java
+++ src/kobe/angariae/AVPlayer.java
@@ -1,46 +1,77 @@
 package kobe.angariae;
 import java.io.IOException;
+import java.util.LinkedList;
 
-import android.media.AudioManager;
-import 	android.media.MediaPlayer;
+import android.media.MediaPlayer;
+import android.net.Uri;
 
 public class AVPlayer {
 
-	private MediaPlayer currentMP;
+	private MediaPlayer mp;
+	//private String[] playlist;
+	private int indexp;
+	private LinkedList<String> playlist;
 	
-	public AVPlayer(String filepath) throws IllegalStateException, IOException{
-		MediaPlayer mediaPlayer = new MediaPlayer();
-		mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-		mediaPlayer.setDataSource(filepath);
-		mediaPlayer.prepare(); // might take long! (for buffering, etc)
-		mediaPlayer.start();
+	public AVPlayer(String[] dirContents){
+		this.playlist = new LinkedList();
+		for(int i=0; i<dirContents.length;i++){
+			this.playlist.add(dirContents[i]);
+		}
+		this.indexp = 0;
 	}
 	
-	public void play(){
-		
+	public void play(String file) throws IllegalArgumentException, SecurityException, IllegalStateException, IOException {
+		mp = new MediaPlayer();
+		mp.setDataSource(file);
+		mp.start();
+	}
+	
+	public void play() throws IllegalArgumentException, SecurityException, IllegalStateException, IOException{
+		mp = new MediaPlayer();
+		mp.setDataSource(playlist.get(indexp));
+		mp.start();
+		indexp++;
+	}
+	
+	public void start(){
+		mp.start();
 	}
 	
 	public void stop(){
-		
+		mp.stop();
 	}
 	
 	public void pause(){
-		
+		mp.pause();
 	}
 	
-	public void next(){
-		
+	public void next() throws IllegalArgumentException, SecurityException, IllegalStateException, IOException{
+		mp = new MediaPlayer();
+		mp.setDataSource(playlist.get(indexp));
+		mp.start();
+		indexp++;
 	}
 	
-	public void previous(){
-		
+	public void previous() throws IllegalArgumentException, SecurityException, IllegalStateException, IOException{
+		indexp--;
+		mp = new MediaPlayer();
+		mp.setDataSource(playlist.get(indexp));
+		mp.start();
 	}
 	
 	public void shuffle(){
 		
 	}
 	
+	public void repeat(){
+		
+	}
+	
 	public void seekTo(){
 	
+	}
+	
+	public LinkedList<String> getPlaylist(){
+		return this.playlist;
 	}
 }
